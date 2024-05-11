90ebd23633c09b92a1bc8de2475b33ec3a7cfef7
ENG Mahmoud Dawod <55116482+mahmouddawod-it@users.noreply.github.com>
2024-05-11 03:04:22 +0200

    Create setup_frappe.sh

---
+++ setup_frappe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the ERPNext repository
+git clone https://github.com/frappe/erpnext.git
+
+# Navigate to the erpnext directory
+cd erpnext
+
+# Run the installation script
+bash setup_frappe.sh --setup-production
